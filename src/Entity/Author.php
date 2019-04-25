b2cf764c228a5bcb5cf59d00058e6decc2786eef
Arnaud <arnaud.lepeltier@cogifor.com>
2019-04-25 12:28:45 +0200

    Add validation on AUthor

--- src/Entity/Author.php
+++ src/Entity/Author.php
@@ -6,6 +6,7 @@ use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 use ApiPlatform\Core\Annotation\ApiResource;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\AuthorRepository")
@@ -26,6 +27,7 @@ class Author
     private $firstName;
 
     /**
+     * @Assert\NotBlank
      * @ORM\Column(type="string", length=255)
      */
     private $lastName;
@@ -79,7 +81,7 @@ class Author
         return $this->lastName;
     }
 
-    public function setLastName(string $lastName): self
+    public function setLastName(?string $lastName): self
     {
         $this->lastName = $lastName;
 
