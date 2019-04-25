1d88a492f8c6bd413a4e3baeb5287825f2f81408
Arnaud <arnaud.lepeltier@cogifor.com>
2019-04-25 12:10:43 +0200

    Add form Extension Date

---
+++ src/Form/Extension/DateExtension.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Form\Extension;
+
+use Symfony\Component\Form\AbstractTypeExtension;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+
+class DateExtension extends AbstractTypeExtension
+{
+    public static function getExtendedTypes():iterable
+    {
+        return [
+            DateType::class
+        ];
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {        
+        $resolver->setDefaults([
+            'widget'=>'single_text',
+            'format'=>'yyyy-MM-dd'
+        ]);
+    }
+}
+
