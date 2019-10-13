cfd5caf63f0a4bcbcdb5e1996ec4a9af024d07a4
Arno <arnaudlepeltier@yahoo.fr>
2019-10-13 14:41:52 +0200

    update dependencies

--- bin/check_code_quality.sh
+++ bin/check_code_quality.sh
@@ -12,7 +12,10 @@ docker-compose exec -u `echo $UID` phpapache vendor/bin/php-cs-fixer fix src --d
 
 docker-compose exec -u `echo $UID` phpapache vendor/bin/phpstan analyse src --level=7
 
-docker-compose exec -u `echo $UID` node node_modules/.bin/retire
+docker-compose exec -u `echo $UID` node node_modules/.bin/retire --ignore .bin
+# /home/app/bin/.phpunit/phpunit-7.5/vendor/phpunit/php-code-coverage/src/Report/Html/Renderer/Template/js/bootstrap.min.js
+#  ↳ bootstrap 4.1.3
+# bootstrap 4.1.3 has known vulnerabilities: severity: high; issue: 28236
 
 docker-compose exec -u `echo $UID` node yarn run eslint webpack.config.js
 
